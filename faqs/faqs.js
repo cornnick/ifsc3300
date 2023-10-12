4f445e7f92349c8d17ba578f9dd3b440511b6a02
cornnick <ntcorn@ualr.edu>
2023-10-12 22:10:29 +0000

    update homepage

--- faqs/faqs.js
+++ faqs/faqs.js
@@ -7,11 +7,14 @@ $(document).ready( () => {
         const h2 = evt.currentTarget;
 
         // toggle minus class for h2 tag
+
         $(h2).toggleClass("minus");
+        $(h2).next().slideToggle(1000);
+
 
         // show or hide related div
         if ($(h2).attr("class") !== "minus") {
-            $(h2).next().hide();
+            $(h2).next().fadeOut(1000);
         }
         else {
            $(h2).next().show();
@@ -19,6 +22,11 @@ $(document).ready( () => {
 
         evt.preventDefault();
     });
+
+    $("#faqs h1").animate(
+        { fontSize: "275%", opacity: 1, left: "+=175" }, 2000,
+        () => $("#faqs h2").next().fadeIn(1000).fadeOut(1000) // callback 
+        );
 
     // set focus on first h2 tag's <a> tag
     $("#faqs").find("a:first").focus();
